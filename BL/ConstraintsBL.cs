f3d0151dadfa6dc8ffc213f7b3bf9ac4d0f3add4
ayalula2000@gmail.com <ayalula2000@gmail.com>
2021-02-06 23:35:50 +0200

    יצירת מחלקות בהתאם למחלקות במסד

---
+++ BL/ConstraintsBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    class ConstraintsBL
+    {
+        //פונקציה לשליפת אילוץ בודד על פי קוד
+
+        //פונקציה לשליפת רשימת אילוצים
+
+        //פונקציה למחיקת אילוץ
+
+        //פונקציה לעדכון אילוץ
+
+        //פונקציה להוספת אילוץ
+    }
+}
---
+++ BL/Employees_RoleBL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    class Employees_RoleBL
+    {
+        //פונקציה לשליפת תפקיד בודד על פי קוד
+
+        //פונקציה לשליפת רשימת תפקידי עובדים
+
+        //פונקציה למחיקת תפקיד
+
+        //פונקציה לעדכון תפקיד
+
+        //פונקציה להוספת תפקיד
+    }
+}
